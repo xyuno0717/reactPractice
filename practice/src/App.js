3a1a6ae340f081851d46fe0746728fffffcf95c8
xyumu56 <xyumu56@gmail.com>
2018-07-26 23:53:39 +0900

    P56,リスト4.4

--- practice/src/App.js
+++ practice/src/App.js
@@ -2,25 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const fullnames = {
-	ryoma: '坂本龍馬',
-	taka: '西郷隆盛',
-	kai: '勝海舟'
-};
-const getFullname = nickname => fullnames[nickname];
+const Hello = () => {
+	return <div>こんにちは、坂本龍馬さん</div>;
+}
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-			<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<h1 className="App-title">Hello World!</h1>
-			</header>
-			<p className="App-intro">
-			To get started, edit <code>src/App.js</code> and save to reload.
-			</p>
-			<p>Hello,{getFullname('ryoma')}</p>
+			<div>
+			<Hello />
+			<Hello />
+			<Hello />
 			</div>
 		);
 	}
